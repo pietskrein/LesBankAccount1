1cefcb7841ceecd7de0c4fd9ae455e34f39b637c
Pieterjan De Bie <pieterjandebie@yahoo.com>
2018-09-28 09:45:49 +0200

    Creeër klasse Bankaccount

---
+++ Bankrekening/Models/Domain/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BankingA1.Models.Domain
+{
+    public class BankAccount
+    {
+
+        #region Fields
+        //private string _accountNumber;
+        //const is sowieso static
+        public const decimal WithdrawCost = 0.10M;
+        private decimal _balance;
+        #endregion
+
+        #region Extra
+        //public decimal GetBalance()
+        //{
+        //    return _balance;
+        //}
+
+        //private void SetBalance(decimal value)
+        //{
+        //    _balance = value;
+        //}
+
+        //Onderstaande lijn is gelijk aan alle voorgaande code 
+        //public decimal Balance { get; private set; } 
+        #endregion
+
+        #region Properties
+        public string AccountNumber { get; private set; }
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("No negative balance allowed");
+                _balance = value;
+            }
+        }
+        #endregion
+
+        //ctrl+k+s om regions te definieren
+        #region Constructors
+
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        //Andere constructor aanroepen
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+        #endregion
+
+        #region Methods
+        //Bevat optionele parameter
+        public void Deposit(decimal amount, int nrOfTime = 1)
+        {
+            Balance += amount * nrOfTime;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost;
+        }
+        #endregion
+    }
+}
--- Bankrekening/Program.cs
+++ Bankrekening/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using BankingA1.Models.Domain;
+using System;
 
-namespace Bankrekening
+namespace BankingA1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Object initializer
+            //BankAccount anotherBA = new BankAccount()
+            //{
+            //    AccountNumber = "123-123-12-99",
+            //    Balance = 200
+            //};
+
+            BankAccount myBa = new BankAccount("123-12312312-99");
+            Console.WriteLine($"Account number is {myBa.AccountNumber}");
+            Console.WriteLine($"Withdraw cost is {BankAccount.WithdrawCost}");
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Deposit(200);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Withdraw(50);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            Console.ReadKey();
         }
     }
 }
